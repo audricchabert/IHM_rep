2155664ddcb82ff59c63e3b48bec9795e3d4ea09
Audric Chabert <achabert@polytech.unice.fr>
2015-12-18 14:02:23 +0100

    can get html data from server

--- app/src/main/java/achabert/vaisseau/GetTask.java
+++ app/src/main/java/achabert/vaisseau/GetTask.java
@@ -1,15 +1,19 @@
 package achabert.vaisseau;
 
+import android.content.Entity;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by achabert on 18/12/2015.
@@ -20,16 +24,29 @@ public class GetTask extends AsyncTask<String,Void,String> {
 
     protected String doInBackground(String... url) {
         HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("http://www.example.com");
+        //HttpGet request = new HttpGet("10.212.121.225:8000");
+        HttpGet request = new HttpGet("http://www.google.com");
         // replace with your url
 
         HttpResponse response;
         try {
             response = client.execute(request);
 
-            Log.d("Response of GET request", response.toString());
+            if(response.getStatusLine().getStatusCode() == 200){
+                HttpEntity entity = response.getEntity();
+                return EntityUtils.toString(response.getEntity());
+            }else{
 
-            return response.toString();
+            }
+
+            //debug
+
+
+            //return response.toString();
+            Log.d("Response of GET request", response.toString());
+            InputStream is = response.getEntity().getContent();
+            return is.toString();
+
         } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -37,6 +54,9 @@ public class GetTask extends AsyncTask<String,Void,String> {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+
+
         return "error";
     }
 
